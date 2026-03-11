28717e3b4e38560c0967ae63506167287da3b9f2
mattfrayser <mattfrayser@gmail.com>
2026-03-11 16:24:48 -0400

    scaffold: CRUD

---
+++ app/Http/Controllers/CanvasController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Canvas;
+use Illuminate\Http\Request;
+
+class CanvasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Canvas $canvas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Canvas $canvas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Canvas $canvas)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
+use Inertia\Inertia;
 
-Route::inertia('/', 'Welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::inertia('dashboard', 'Dashboard')->name('dashboard');
-});
+Route::get('/', function () {
+    return inertia('Dashboard');
+})->name('home');
 
 require __DIR__.'/settings.php';
